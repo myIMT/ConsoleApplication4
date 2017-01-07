ba78c74a5205ba905b316a484006af6c884d8da1
jodi319 <jodi319@gmail.com>
2017-01-07 03:00:24 +0200

    connectedComponent with findContours - problem

--- ConsoleApplication4/findContours.cpp
+++ ConsoleApplication4/findContours.cpp
@@ -1,42 +1,42 @@
-////#include "opencv2/core/core.hpp"
-////#include "opencv2/highgui/highgui.hpp"
-////#include "opencv2/imgproc/imgproc.hpp"
-////#include "iostream"
-////using namespace cv;
-////using namespace std;
-////int main()
-////{
-////	Mat image;
-////	image = imread("20161215 02.33_368L.jpg", 1);
-////	namedWindow("Display window", CV_WINDOW_AUTOSIZE);
-////	imshow("Display window", image);
-////	Mat gray;
-////	cvtColor(image, gray, CV_BGR2GRAY);
-////	Canny(gray, gray, 100, 200, 3);
-////	/// Find contours   
-////	vector<vector<Point> > contours;
-////	vector<Vec4i> hierarchy;
-////	RNG rng(12345);
-////	findContours(gray, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-////	/// Draw contours
-////	Mat drawing = Mat::zeros(gray.size(), CV_8UC3);
-////	for (int i = 0; i< contours.size(); i++)
-////	{
-////		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-////		drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
-////	}
+//////#include "opencv2/core/core.hpp"
+//////#include "opencv2/highgui/highgui.hpp"
+//////#include "opencv2/imgproc/imgproc.hpp"
+//////#include "iostream"
+//////using namespace cv;
+//////using namespace std;
+//////int main()
+//////{
+//////	Mat image;
+//////	image = imread("20161215 02.33_368L.jpg", 1);
+//////	namedWindow("Display window", CV_WINDOW_AUTOSIZE);
+//////	imshow("Display window", image);
+//////	Mat gray;
+//////	cvtColor(image, gray, CV_BGR2GRAY);
+//////	Canny(gray, gray, 100, 200, 3);
+//////	/// Find contours   
+//////	vector<vector<Point> > contours;
+//////	vector<Vec4i> hierarchy;
+//////	RNG rng(12345);
+//////	findContours(gray, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+//////	/// Draw contours
+//////	Mat drawing = Mat::zeros(gray.size(), CV_8UC3);
+//////	for (int i = 0; i< contours.size(); i++)
+//////	{
+//////		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+//////		drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
+//////	}
+//////
+//////	imshow("Result window", drawing);
+//////	waitKey(0);
+//////	return 0;
+//////}
 ////
-////	imshow("Result window", drawing);
-////	waitKey(0);
-////	return 0;
-////}
-//
-//
-//
-//
-//
-//
-//
+////
+////
+////
+////
+////
+////
 //#include "opencv2/core/core.hpp"
 //#include "opencv2/highgui/highgui.hpp""
 //#include "opencv2/imgproc/imgproc.hpp"
@@ -46,7 +46,7 @@
 //int main()
 //{
 //	Mat src;
-//	src = imread("images5.jpg", CV_LOAD_IMAGE_COLOR);
+//	src = imread("mask_i.jpg", CV_LOAD_IMAGE_COLOR);
 //	Mat gray;
 //	cvtColor(src, gray, CV_BGR2GRAY);
 //	threshold(gray, gray, 200, 255, THRESH_BINARY_INV); //Threshold the gray
@@ -71,7 +71,7 @@
 //		drawContours(src, contours, largest_contour_index, color, CV_FILLED, 8, hierarchy);
 //		rectangle(src, bounding_rect, Scalar(0, 255, 0), 2, 8, 0);
 //		//namedWindow(name, CV_WINDOW_AUTOSIZE);
-//		//imshow(name, src);
+//		imshow(name, src);
 //		imwrite(name + ".jpg", src);
 //		if (a>largest_area) {
 //			largest_area = a; cout << i << " area  " << a << endl;
